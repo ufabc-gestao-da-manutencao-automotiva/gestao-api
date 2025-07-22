4e08c30e1958f92d4bb4ea9ea899be09a281761f
Fabio_t <fabio.silva.teixeira1@gmail.com>
2025-07-21 22:11:48 -0300

    first api test

---
+++ api/index.ts
@@ -0,0 +1,17 @@
+import Fastify, { FastifyRequest, FastifyReply } from "fastify";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.get("/", async (req, reply) => {
+  return reply.status(200).send({ message: "Hello!" });
+});
+
+export default async function handler(
+  req: FastifyRequest,
+  reply: FastifyReply
+) {
+  await app.ready();
+  app.server.emit("request", req, reply);
+}
