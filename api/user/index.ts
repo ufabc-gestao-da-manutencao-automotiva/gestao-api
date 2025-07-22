6a3cc022af18e3ac2c39bb543ad6b1d651851b42
Fabio_t <fabio.silva.teixeira1@gmail.com>
2025-07-22 00:49:37 -0300

    create user api

--- api/user/index.ts
+++ api/user/index.ts
@@ -10,13 +10,12 @@ app.get("/api/user", async (req, reply) => {
   return reply.status(200).send(users);
 });
 
-app.get("/api/users", async (req, reply) => {
-  const users = await prisma.user.findMany();
-  return reply.status(200).send(users);
-});
-
 app.post("/api/user", async (req, reply) => {
   const { name, age } = req.body as any;
+
+  if (!age){
+      return reply.status(400).send({ message: "É obrigatório informar idade" });
+}
 
   try {
     const user = await prisma.user.create({
@@ -31,6 +30,16 @@ app.post("/api/user", async (req, reply) => {
     return reply.status(400).send({ message: JSON.stringify(error) });
   }
 });
+
+
+
+//já fiz o get/Read e o post/Create
+//agora falta o put/Update e o delete/Delete
+
+//dpois fazer o get, post, put e delete das outras collections
+
+//
+
 
 export default async function handler(
   req: FastifyRequest,
